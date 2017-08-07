b7c07f47351dc25c6b553db535fca22833a36c09
Thiago Pereira Martins <thiago.martins@nddigital.local>
2017-08-07 16:01:04 -0300

    Alterando modo view

--- app/js/views/GeradorView.js
+++ app/js/views/GeradorView.js
@@ -1,36 +1,11 @@
+const fs = require('fs');
 class GeradorView{
     constructor(elemento){
         this._elemento = elemento;
     }
     _template(){
-        return `
-           <div class="col s12 center-align">
-            <form action="#">
-              <div class="file-field input-field">
-                <div class="btn">
-                  <span>Origem</span>
-                  <input type="file">
-                </div>
-                <div class="file-path-wrapper">
-                  <input class="file-path validate" type="text">
-                </div>
-              </div>
-            </form>
-          </div>
-          <div class="col s12 center-align">
-            <form action="#">
-              <div class="file-field input-field">
-                <div class="btn">
-                  <span>Destino</span>
-                  <input type="file">
-                </div>
-                <div class="file-path-wrapper">
-                  <input class="file-path validate" type="text">
-                </div>
-              </div>
-            </form>
-          </div>             
-        `;
+        let html = fs.readFileSync('./views/GeradorView.html', 'utf8');
+        return html;
     }
     update(){
         this._elemento.innerHTML = this._template();
