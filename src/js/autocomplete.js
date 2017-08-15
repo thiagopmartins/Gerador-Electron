9e2e44a0bf937eac3c49b5eee452d00194251539
Diego de Liz <diego.liz@nddigital.local>
2017-08-15 09:02:45 -0300

    Alteração no layout e autocomplete

---
+++ src/js/autocomplete.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    var window_width = $(window).width();
+
+    $('input.autocomplete').autocomplete({
+      data: {"06255692000103": null, "10280765000186": null, "6168561": null},
+    });
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
--- src/modal.js
+++ src/modal.js
@@ -8,12 +8,6 @@ window.onload = function(){
     conteudo = fs.readFileSync('./data/arquivo.tmp', 'utf8');
     $('#textarea1').value = conteudo;
 };
-$('#btnSalvarNota').onclick = () => {
-    ArquivoBaseModel.salvarArquivo = $('#textarea1').value;
-    document.getElementById('notificacao').style.transition = 'color 2s ease 2s';
-    document.getElementById('notificacao').style.display = 'block';
-    document.getElementById('notificacao').innerHTML = "Arquivo salvo com sucesso";
-};
 $('#textarea1').oninput = () =>{
     if(conteudo == $('#textarea1').value){
         $('#btnDescartar').classList.add('disabled'); 
@@ -27,11 +21,17 @@ $('#textarea1').oninput = () =>{
     console.log($('#textarea1').value); 
     
 };
+$('#btnSalvarNota').onclick = () => {
+    ArquivoBaseModel.salvarArquivo = $('#textarea1').value;
+    document.getElementById('notificacao').style.display = 'block';
+    document.getElementById("notificacao").className = "col s8 chip offset-s2 green";
+    document.getElementById('notificacao').innerHTML = "Arquivo salvo com sucesso";
+};
 $('#btnDescartar').onclick = () => {
     $('#textarea1').value = conteudo;
     ArquivoBaseModel.salvarArquivo = conteudo;
-    document.getElementById('notificacao').style.transition = 'color 2s ease 2s';
     document.getElementById('notificacao').style.display = 'block';
+    document.getElementById("notificacao").className = "col s8 chip offset-s2 red";
     document.getElementById('notificacao').innerHTML = "As alterações foram desfeitas";
 }
 $('#btnRestaurar').onclick = () => {
@@ -43,8 +43,8 @@ $('#btnRestaurar').onclick = () => {
             console.log(conteudoOrigem); 
             $('#textarea1').value = conteudoOrigem;
             ArquivoBaseModel.salvarArquivo = conteudoOrigem;
-            document.getElementById('notificacao').style.transition = 'color 2s ease 2s';
             document.getElementById('notificacao').style.display = 'block';
+            document.getElementById("notificacao").className = "col s8 chip offset-s2 yellow";
             document.getElementById('notificacao').innerHTML = "Arquivo restaurado com sucesso";            
         },(erro) =>{ console.log(erro); }
      );        
