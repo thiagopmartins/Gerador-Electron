1e60bed8257b8a49ca681c37b69c3ec44c6ff455
Diego de Liz <diego.liz@nddigital.local>
2017-08-25 10:39:27 -0300

    Autocomplete

--- src/js/autocomplete.js
+++ src/js/autocomplete.js
@@ -4,7 +4,7 @@
     var window_width = $(window).width();
 
     $('input.autocomplete').autocomplete({
-      data: { "06255692000103": null, "10280765000186": null, "6168561": null },
+      data: { "06255692000103": 'NDD', "10280765000186": 'Tumelero', "42274696004262": 'Adidas', "02043800000115": 'Pro Audio', "47508411022559": 'GPA' },
     });
 
   }); // end of document ready
