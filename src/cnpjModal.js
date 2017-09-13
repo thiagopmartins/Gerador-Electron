2445837fd0fdb48baaa6e6c735699f1a6edcbcd6
Diego de Liz <diego.liz@hotmail.com>
2017-09-12 23:15:47 -0300

    Salvar cnpj

--- src/cnpjModal.js
+++ src/cnpjModal.js
@@ -1,8 +1,17 @@
 const CnpjModel = require('./js/model/CnpjModel.js');
 
 let cnpjModel;
+let $ = document.querySelector.bind(document);
 
 window.onload = function(){
-    let $ = document.querySelector.bind(document);
     cnpjModel = new CnpjModel();
+};
+
+$('#btnSalvarCnpj').onclick = () => {
+    let cnpj = document.getElementById('cnpj');
+    let empresa = document.getElementById('empresa');
+    cnpjModel.salvarArquivo(cnpj, empresa);
+    document.getElementById('notificacao').style.display = 'block';
+    document.getElementById("notificacao").className = "col s8 chip offset-s2 green";
+    document.getElementById('notificacao').innerHTML = "Arquivo salvo com sucesso";
 };
--- src/js/model/CnpjModel.js
+++ src/js/model/CnpjModel.js
@@ -1,7 +1,40 @@
+const fs = require("fs");
+
 let data;
-class CnpjModal{    
-    constructor(){
-        
-    }
+class CnpjModal {
+  constructor(cnpj, empresa) {
+    this.cnpj = cnpj;
+    this.empresa = empresa;
+  }
+
+  salvarArquivo() {
+    const config = {
+      user: "sa",
+      password: "p@ssw0rd",
+      server: "172.31.40.181", // You can use 'localhost\\instance' to connect to named instance
+      database: "NDD_CONNECTOR_NFCe",
+
+      options: {
+        encrypt: true // Use this if you're on Windows Azure
+      },
+
+      connectionString:
+        "Driver={SQL Server Native Client 11.0};Server=172.31.40.181\\sql;Database=NDD_CONNECTOR_NFCe;Uid=sa;Pwd=p@ssw0rd;"
+    };
+
+    const sql = require("mssql");
+
+    new sql.ConnectionPool(config)
+      .connect()
+      .then(pool => {
+        return pool.query`INSERT into TBCNPJ cnpj, empresa VALUES ($cnpj, $empresa)`;
+      })
+      .then(result => {
+        return "CNPJ salvo com sucesso";
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 }
 module.exports = CnpjModal;
