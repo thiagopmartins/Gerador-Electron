b4d5d4a1a4e1041894bd3f47449803695fc0210e
Thiago Pereira Martins <thiago.martins@nddigital.local>
2017-11-20 11:19:58 -0200

    mysql2

--- gerador-massas-win32-x64/resources/app/src/employeesModal.js
+++
@@ -1,9 +0,0 @@
-const EmployeesModel = require('./js/model/EmployeesModel.js');
-
-let employeesModel;
-
-window.onload = function(){
-    let $ = document.querySelector.bind(document);
-    employeesModel = new EmployeesModel();
-    employeesModel.funcionarios;
-};
