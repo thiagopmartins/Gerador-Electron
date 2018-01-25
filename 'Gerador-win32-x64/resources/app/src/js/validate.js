2fa4af6e1d9d54279fe7a9193f61918cfbc0a19c
Thiago Pereira Martins <thiago.martins@nddigital.local>
2018-01-25 08:56:08 -0200

    Att Electron

--- 'Gerador-win32-x64/resources/app/src/js/validate.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-    $("input").keyup(function () {
-        if ($("input").hasClass("invalid")) {
-            $('#gerarNotas').addClass('disabled');
-        } else {
-            $('#gerarNotas').removeClass('disabled');
-        }
-    });
-})(jQuery); // end of jQuery name space
